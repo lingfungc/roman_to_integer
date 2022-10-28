d5160746b7de2f25cc73e6cc8f51db64cd25b969
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-28 17:08:09 +0100

    completed the roman_to_int method

--- roman_to_integer.rb
+++ roman_to_integer.rb
@@ -11,11 +11,22 @@ ROMAN_NUM = {
 
 def roman_to_int(string)
   sum = 0
-
   roman_numbers = string.chars
-  roman_numbers.each do |num|
-    sum += ROMAN_NUM[num]
+  # convert each roman_number to integer and store in an array
+  numbers_value = roman_numbers.map { |num| ROMAN_NUM[num] }
+
+  numbers_value.each_with_index do |num, index|
+    # compare each integer with the next one, except the last integer
+    if !numbers_value[index + 1].nil? && num < numbers_value[index + 1]
+      # 'IV' = sum - 'I' + 'V'
+      sum -= num
+    else
+      sum += num
+    end
   end
+  sum
 end
 
-p roman_to_int('III')
+# p roman_to_int('III')
+# p roman_to_int('LVIII')
+# p roman_to_int('MCMXCIV')
